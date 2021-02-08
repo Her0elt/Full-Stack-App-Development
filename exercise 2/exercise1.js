7dc6c5610c982f50d406196572dcf96cf51f3813
Hermann Elton <60223374+Her0elt@users.noreply.github.com>
2021-02-08 23:56:16 +0100

    ish:

--- exercise 2/exercise1.js
+++
@@ -1,9 +0,0 @@
-// const url = '' 
-// const display = document.querySelector("#display")
-// fetch(url).then((res) =>{
-//     res.json().then((data) =>{
-//         display.innerHTML = data
-//     })
-// })
-
-console.log('hello world')
